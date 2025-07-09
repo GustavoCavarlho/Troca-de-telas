980281173a74a685d1e3021074c2d451a7e5d9d1
GustavoCavarlho <carvalho.souza28@escola.pr.gov.br>
2025-07-09 09:18:12 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './HomeScreem.js';
+import SecondaryScreen from './screen/SecondaryScreen';
+import CafeScreen from './screen/CafeScreen';
+import BoloScreen from './screen/BoloScreen';
+import PaoScreen from './screen/PaoScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Bem-Vindo!' }}/>
+        <Stack.Screen name='Secondary' component={SecondaryScreen} options={{ title: 'Opções Deliciosas' }}/>
+        <Stack.Screen name='Cafe' component={CafeScreen} options={{ title: 'Nosso Café' }}/>
+        <Stack.Screen name='Bolo' component={BoloScreen} options={{ title: 'Bolo de Chocolate' }}/>
+        <Stack.Screen name='Pao' component={PaoScreen} options={{ title: 'Pão Caseiro' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
